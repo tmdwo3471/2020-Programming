c8d281924eaceb591cbd084938dd5039ed029fb2
tmdwo3471 <tmdwo3471@naver.com>
2020-11-30 01:50:43 +0900

    수정 upload

---
+++ code ch09/inputarray.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int input[20] = { 0 };
+
+	printf("배열에 저장할 정수를 여러 개 입력하시오.");
+	printf(" 0을 입력하면 입력을 종료합니다.\n");
+	int i = 0;
+	do {
+		scanf_s("%d", &input[i]);
+	} while (input[i++] != 0);
+
+	i = 0;
+	while (input[i] != 0); {
+		printf("%d ", input[i++]);
+	}
+	puts("");
+
+	return 0;
+}
---
+++ code ch10/char.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <ctype.h>
+
+void print2char(char);
+
+int main(void)
+{
+	char ch;
+
+	printf("알파벳(종료X) 또는 다른 문자 입력하세요.\n");
+
+	do
+	{
+		printf("문자 입력 후 Enter: ");
+		scanf_s("%c", &ch);
+		getchar();
+		if (isalpha(ch))
+			print2char(ch);
+		else
+			printf("입력: %c\n", ch);
+	} while (ch != 'x' && ch != 'X');
+
+	return 0;
+}
+void print2char(char ch)
+{
+	if (isupper(ch))
+		printf("입력: %c, 변환: %c\n", ch, tolower(ch));
+	else
+		printf("입력: %c, 변환: %c\n", ch, toupper(ch));
+
+	return;
+}
